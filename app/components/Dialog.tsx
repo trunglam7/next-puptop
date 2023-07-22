ebae38a8c9974d7804e998466ab8e6e45db3542a
Trung Lam <trunglam107@gmail.com>
2023-07-22 08:24:32 -0700

    finished ui form

--- app/components/Dialog.tsx
+++ app/components/Dialog.tsx
@@ -1,10 +1,33 @@
-import React, { useContext } from 'react'
+import React, { use, useContext, useRef, useState } from 'react'
 import styles from '../styles/dialog.module.css'
 import { dialogContext } from '../page';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 
 export default function Dialog() {
 
   const {dialogState, setDialogState} = useContext(dialogContext);
+  const [dogName, setDogName]= useState('');
+  const [dogImage, setDogImage] = useState<File | null>(null);
+  const dogImageInput = useRef<HTMLInputElement>(null);
+
+  const addDog = useMutation(api.dogs.addDog);
+
+  const addDogHandler = async () => {
+
+    await addDog({name: dogName, score: 0, image: "duhuwhduwdhudwh"});
+
+    const sendImageUrl = new URL(`${process.env.NEXT_PUBLIC_CONVEX_URL!}/${dogName}`);
+
+    await fetch(sendImageUrl, {
+      method: "POST",
+      headers: {"Content-Type": dogImage!.type},
+      body: dogImage
+    });
+
+    setDogImage(null);
+    dogImageInput.current!.value = "";
+  }
 
   const closeDialog = () => {
     setDialogState(false);
@@ -12,7 +35,22 @@ export default function Dialog() {
 
   return (
     <dialog className={styles.dialogContainer} open={dialogState}>
-      <button onClick={closeDialog}>Close</button>
+      <div className={styles.formWrapper}>
+        <div className={styles.formContainer}>
+          <div className={styles.inputContainer}>
+            <label htmlFor='dog-name'>Dog Name</label>
+            <input aria-label='dog name' placeholder='Name' id='dog-name' onChange={e => setDogName(e.target.value)}></input>
+          </div>
+
+          <div className={styles.inputContainer}>
+            <label htmlFor='dog-image'>Dog Image</label>
+            <input type='file' id='dog-image' ref={dogImageInput} onChange={e => setDogImage(e.target.files![0])}></input>
+          </div>
+
+          <button onClick={addDogHandler}>Submit</button>
+          <button onClick={closeDialog}>Close</button>
+        </div>
+      </div>
     </dialog>
   )
 }
